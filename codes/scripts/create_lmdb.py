39b9c4786220b801c8d4cf699d015011beb25dba
Ekaterina Lezine <31071818+elezine@users.noreply.github.com>
2020-04-28 14:58:24 -0400

    Update create_lmdb.py

--- codes/scripts/create_lmdb.py
+++ codes/scripts/create_lmdb.py
@@ -12,8 +12,8 @@ except ImportError:
     pass
 
 # configurations
-img_folder = '/home/ekaterina_lezine/esrgan_test_data/DIV2K/train_mod/HR/x4/*.png'  # glob matching pattern
-lmdb_save_path = '/home/ekaterina_lezine/esrgan_test_data/DIV2K/train_mod/train_HR.lmdb'
+img_folder = '/home/ekaterina_lezine/esrgan_test_data/DIV2K/train_mod/LR/x4/*.png'  # glob matching pattern
+lmdb_save_path = '/home/ekaterina_lezine/esrgan_test_data/DIV2K/train_mod/train_LR.lmdb'
 meta_info = {'name': 'DIV2K800_sub_GT'}
 mode = 2  # 1 for reading all the images to memory and then writing to lmdb (more memory);
 # 2 for reading several images and then writing to lmdb, loop over (less memory)
