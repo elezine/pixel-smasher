80885c4b635a1476281fc6dec0e1336a3f962418
Ekaterina Lezine <31071818+elezine@users.noreply.github.com>
2020-05-05 18:55:26 -0400

    esrgan

--- codes/train.py
+++ codes/train.py
@@ -75,6 +75,7 @@ def main():
         logger = logging.getLogger('base')
         logger.info(option.dict2str(opt))
         # tensorboard logger
+
         if opt['use_tb_logger'] and 'debug' not in opt['name']:
             version = float(torch.__version__[0:3])
             if version >= 1.1:  # PyTorch 1.1
@@ -84,6 +85,7 @@ def main():
                     'You are using PyTorch {}. Tensorboard will use [tensorboardX]'.format(version))
                 from tensorboardX import SummaryWriter
             tb_logger = SummaryWriter(log_dir='../tb_logger/' + opt['name'])
+        
     else:
         util.setup_logger('base', opt['path']['log'], 'train', level=logging.INFO, screen=True)
         logger = logging.getLogger('base')
