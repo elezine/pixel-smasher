b5fc0f09074a40648299067ab94954f14637bc4c
Ethan (milestones) <ethan_kyzivat@brown.edu>
2020-05-06 14:05:04 +0000

    checkpoint: callback returns None

--- codes/classification/classifier.py
+++ codes/classification/classifier.py
@@ -20,7 +20,7 @@ outdir='/data_dir/classify/valid_mod'
 up_scale=4
 iter=100000 # quick fix to get latest validation image in folder
 
-def group_classify(sourcedir_SR, sourcedir_R, outdir, name, threshold=10): # filstrucutre is pre-defined
+def group_classify(i, sourcedir_SR, sourcedir_R, outdir, name, threshold=10): # filstrucutre is pre-defined
     '''
     A simple classification function for high-resolution, low-resolution, and  super resolution images.  Takes input path and write To output path (pre-– formatted).
     '''
@@ -39,7 +39,7 @@ def group_classify(sourcedir_SR, sourcedir_R, outdir, name, threshold=10): # fil
 
         # run classification procedure
     # if 
-    if os.path.isfile(Bic_out_pth)==False: # only write if file doesn't exist\
+    if 1==1: #os.path.isfile(Bic_out_pth)==False: # only write if file doesn't exist\ # HERE change back
         print('No.{} -- Classifying {}'.format(i, name)) # printf: end='' # somehow i is in this functions namespace...?
         classify(SR_in_pth, SR_out_pth)
         classify(HR_in_pth, HR_out_pth)
@@ -49,16 +49,22 @@ def group_classify(sourcedir_SR, sourcedir_R, outdir, name, threshold=10): # fil
         print('Skipping: {}.'.format(name))
     # save out put to row
 
-def classify(pth_in, pth_out, threshold=10):
+def classify(pth_in, pth_out, threshold=2):
         # classify procedure
     img = cv2.imread(pth_in, cv2.IMREAD_UNCHANGED) # HERE change
     ndvi = np.array((img[:,:,0]-img[:,:,2])/(img[:,:,0]+img[:,:,2]), dtype='int16')
     bw=ndvi>threshold # output mask from classifier
         # count pixels
-
+    nWaterPix=np.sum(bw)
         # write out
     cv2.imwrite(pth_out, np.array(255*bw, 'uint8'))
-    pass # TODO
+    #pass # Why is this necessary?  It's not
+    return nWaterPix
+
+def collect_result(result):
+    global results
+    results.append(result)
+
 if __name__ == '__main__':
 
         # for testing
@@ -72,12 +78,14 @@ if __name__ == '__main__':
         # loop over files
     dirpaths = [f for f in os.listdir(sourcedir_SR) ] # removed: if f.endswith('.png')
     num_files = len(dirpaths)
+    # global results
+    results = [] # init
     pool = Pool(mp.cpu_count())
     for i in range(30): #range(num_files): # switch for testing
         name = dirpaths[i]
 
         # parallel
-        pool.apply_async(group_classify, args=(sourcedir_SR, sourcedir_R, outdir, name, 10)) # , callback=update
+        pool.apply_async(group_classify, args=(i, sourcedir_SR, sourcedir_R, outdir, name, 10), callback=collect_result) # , callback=update
     pool.close()
     pool.join()
     print('All subprocesses done.')
