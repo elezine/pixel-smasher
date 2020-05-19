881e3525ea84d7de1ae1ea4a5f834303a5afbe05
Ethan (milestones) <ethan_kyzivat@brown.edu>
2020-05-19 19:17:38 +0000

    working plots w 3 choice images

--- codes/plotting/plot_images.py
+++ codes/plotting/plot_images.py
@@ -11,17 +11,20 @@ import pickle
 from scipy import stats as st
 import skimage as ski
 import matplotlib.pyplot as plt
+from matplotlib.colors import Normalize
 
 #%% I/O
 sourcedir_SR='/data_dir/pixel-smasher/experiments/003_RRDB_ESRGANx4_PLANET/val_images'
 sourcedir_R='/data_dir/valid_mod'
 classdir='/data_dir/classify/valid_mod'
 up_scale=4
-current_thresh=0.1
+current_thresh=0.2
 iter=60000 # quick fix to get latest validation image in folder
-names=['622194_1368808_2017-07-14_102d_BGRN_Analytic_s0562', '622194_1368808_2017-07-14_102d_BGRN_Analytic_s0699']
-# 660535_1066622_2017-08-01_1041_BGRN_Analytic_s1002 622194_1368808_2017-07-14_102d_BGRN_Analytic_s0562 581200_1368610_2017-06-26_1030_BGRN_Analytic_s0389 584870_1368808_2017-06-27_1038_BGRN_Analytic_s0110
+rescale_factor=8 # quick and dirty rescale
+names=['622194_1368808_2017-07-14_102d_BGRN_Analytic_s0562', '622194_1368808_2017-07-14_102d_BGRN_Analytic_s0699', '584870_1368808_2017-06-27_1038_BGRN_Analytic_s0110']
+#    
 
+# clouds: 660535_1066622_2017-08-01_1041_BGRN_Analytic_s1002 660535_1066622_2017-08-01_1041_BGRN_Analytic_s1002 581200_1368610_2017-06-26_1030_BGRN_Analytic_s0389
 #%% loop
 fig3, axes = plt.subplots(nrows=len(names),ncols=5, figsize=plt.figaspect(len(names)/5*1))
 
@@ -35,25 +38,25 @@ for i in range(len(names)):
     class_SR_in_pth=os.path.join(classdir, 'SR', 'x' + str(up_scale), names[i]+'_T'+str(current_thresh)+ '.png')
 
     # load
-    SR_in = cv2.imread(SR_in_pth, cv2.IMREAD_COLOR).astype('float')
-    HR_in = cv2.imread(HR_in_pth, cv2.IMREAD_COLOR).astype('float')
-    LR_in = cv2.imread(LR_in_pth, cv2.IMREAD_COLOR).astype('float')
-    Bic_in = cv2.imread(Bic_in_pth, cv2.IMREAD_COLOR).astype('float')
-    class_SR_in = cv2.imread(class_SR_in_pth, cv2.IMREAD_UNCHANGED).astype('float')
+    SR_in = cv2.imread(SR_in_pth)
+    HR_in = cv2.imread(HR_in_pth)
+    LR_in = cv2.imread(LR_in_pth)
+    Bic_in = cv2.imread(Bic_in_pth)
+    class_SR_in = cv2.imread(class_SR_in_pth, cv2.IMREAD_UNCHANGED) # load as greyscale
     # class_HR_in = cv2.imread(SR_in_pth, cv2.IMREAD_UNCHANGED)
 
 # TODO: plot accuracy metrics, too
-    axes[i,0].imshow(cv2.normalize(SR_in, None, norm_type=cv2.NORM_MINMAX)) # norme=
-    axes[i,1].imshow(cv2.normalize(HR_in, None, norm_type=cv2.NORM_MINMAX))
-    axes[i,2].imshow(cv2.normalize(LR_in, None, norm_type=cv2.NORM_MINMAX))
-    axes[i,3].imshow(cv2.normalize(Bic_in, None, norm_type=cv2.NORM_MINMAX))
-    axes[i,4].imshow(cv2.normalize(class_SR_in, None, norm_type=cv2.NORM_MINMAX), cmap='Greys')
+    axes[i,0].imshow(SR_in*rescale_factor) # norme=
+    axes[i,1].imshow(HR_in*rescale_factor)
+    axes[i,2].imshow(LR_in*rescale_factor)
+    axes[i,3].imshow(Bic_in*rescale_factor)
+    axes[i,4].imshow(class_SR_in, cmap='Greys')
     
     for j in range(5):
         # plot
         # fig=plt.figure()
         # fig.add_subplot(251)
-        # axes[i,j].imshow(cv2.normalize(SR_in, cv2.IMREAD_COLOR, norm_type=cv2.NORM_MINMAX))
+        # axes[i,j].imshow(cv2.normalize(SR_in, norm_type=cv2.NORM_MINMAX))
         # axes[i,j].imshow(cv2.normalize(SR_in, None, 1.0, 0.0, cv2.NORM_L1))
         # axes.tick_params...
         # fig3.subplots_adjust(wspace=0.5, hspace=0.3, left=0.125, right=0.9, top=0.9, bottom=0.1)
@@ -65,3 +68,5 @@ fig3.tight_layout()
 plt.savefig('fig3')
 print('done')
     
+
+# %%
