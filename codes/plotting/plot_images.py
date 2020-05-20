3fd20db6fd7dff66af8df695314b80a5729b799a
Ethan (milestones) <ethan_kyzivat@brown.edu>
2020-05-20 02:20:55 +0000

    added zoomed out figures

--- codes/plotting/plot_images.py
+++ codes/plotting/plot_images.py
@@ -75,11 +75,61 @@ for i in range(len(names)):
             axes[j,i].set(ylabel=ylabels[j])
         if j==0:
             axes[j,i].set(title=labels[i]) # (title=names[i]
+#%% second loop
+za=(0,0,0) #100 # zoom bound A
+zb=(480, 480, 480) #300; # zoom bound 
 
+fig2, axes2 = plt.subplots(ncols=len(names),nrows=5, figsize=plt.figaspect(5/len(names)*1))
+
+for i in range(len(names)):
+        
+    # paths
+    SR_in_pth=os.path.join(sourcedir_SR,names[i],names[i]+'_'+str(iter)+'.png')
+    HR_in_pth=os.path.join(sourcedir_R, 'HR', 'x' + str(up_scale), names[i]+ '.png')
+    LR_in_pth=os.path.join(sourcedir_R, 'LR', 'x' + str(up_scale), names[i]+ '.png')
+    Bic_in_pth=os.path.join(sourcedir_R, 'Bic', 'x' + str(up_scale), names[i]+ '.png')
+    class_SR_in_pth=os.path.join(classdir, 'SR', 'x' + str(up_scale), names[i]+'_T'+str(current_thresh)+ '.png')
+
+    # load
+    SR_in = cv2.imread(SR_in_pth)
+    HR_in = cv2.imread(HR_in_pth)
+    LR_in = cv2.imread(LR_in_pth)
+    Bic_in = cv2.imread(Bic_in_pth)
+    class_SR_in = cv2.imread(class_SR_in_pth, cv2.IMREAD_UNCHANGED) # load as greyscale
+    # class_HR_in = cv2.imread(SR_in_pth, cv2.IMREAD_UNCHANGED)
+
+# TODO: plot accuracy metrics, too
+    axes2[0,i].imshow(HR_in[za[i]:zb[i],za[i]:zb[i]:]*rescale_factor)
+    axes2[1,i].imshow(LR_in[np.int(za[i]/up_scale):np.int(zb[i]/up_scale),np.int(za[i]/up_scale):np.int(zb[i]/up_scale):]*rescale_factor)
+    axes2[2,i].imshow(Bic_in[za[i]:zb[i],za[i]:zb[i]:]*rescale_factor) # <----- HERE: fix scaling for LR !
+    axes2[3,i].imshow(SR_in[za[i]:zb[i],za[i]:zb[i]:]*rescale_factor, extent=(100.5, 150.5, 150.5,100.5)) # norm=
+    axes2[4,i].imshow(class_SR_in[za[i]:zb[i],za[i]:zb[i]:], cmap='Greys')
+    
+    
+
+    for j in range(5):
+        # plot
+        # fig=plt.figure()
+        # fig.add_subplot(251)
+        # axes[i,j].imshow(cv2.normalize(SR_in, norm_type=cv2.NORM_MINMAX))
+        # axes[i,j].imshow(cv2.normalize(SR_in, None, 1.0, 0.0, cv2.NORM_L1))
+        # axes.tick_params...
+        # fig1.subplots_adjust(wspace=0.5, hspace=0.3, left=0.125, right=0.9, top=0.9, bottom=0.1)
+        
+        # axes[j,i].axis('off')
+        axes2[j,i].xaxis.set(ticks=(), ticklabels=())
+        axes2[j,i].yaxis.set(ticks=(), ticklabels=())
+        # plt.axis('off')
+        if i==0:
+            axes2[j,i].set(ylabel=ylabels[j])
+        if j==0:
+            axes2[j,i].set(title=labels[i]) # (title=names[i]
 #%% plot and save
 fig1.tight_layout()
-fig1.show()
+fig2.tight_layout()
+# fig1.show()
 fig1.savefig('Fig_subsets.png', dpi=300)
+fig2.savefig('Fig_subsets_zoom_out.png', dpi=300)
 print('done')
     
 
