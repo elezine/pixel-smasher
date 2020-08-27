8198dff0e56d7b4bbdbac68c4d4144bbc1b02b07
Ethan (milestones) <ethan_kyzivat@brown.edu>
2020-08-27 20:17:00 +0000

    Modified to use, even though I didn't plan on needing it.

--- codes/utils/train_val_split.sh
+++ codes/utils/train_val_split.sh
@@ -1,17 +1,22 @@
 # bash script to move files from generate_mod_LR_bic output to train / val folders
 
-VALSIZE=1000 # number of validation files (rest for training)
+# use after: find . -type f -iname "*.png"  |  cut -c 3- > ../all10.txt
+
+# VALSIZE=1000 # number of validation files (rest for training)
 #shuf all1.txt -n 1000 > valid_shuf1.txt
-basepath=/data_dir/planet_sub_LR_cal
-suffix=x8
+basepath=/data_dir/train_mod/
+basepath_out=/data_dir/valid_mod # hold valid
+shuf_list=/data_dir/planet_sub/valid_shuf10.txt # hold valid
+# all_list=/data_dir/planet_sub/all10.txt # hold valid
+suffix=x4
 
 for folder in Bic  HR  LR; do
     input=$basepath/$folder/$suffix
     echo $input
-    for mv_folder in /data_dir/valid_mod_cal/$folder/$suffix; do # /data_dir/train_mod_cal
+    for mv_folder in $basepath_out/$folder/$suffix; do # /data_dir/train_mod_cal
         echo mv folder: $mv_folder
-        #mkdir -p $mv_folder
-        #cat valid_shuf.txt | while read file; do mv $input/$file $mv_folder/$file; done
+        # mkdir -p $mv_folder
+        cat $shuf_list | while read file; do mv $input/$file $mv_folder/$file; done
         # # mv $input/*.png /data_dir/train_mod_cal/$folder/$suffix/$file # too long...
         # mkdir -p train_mod_cal/$folder/$suffix
         # ls $input  | while read file; do mv $input/$file /data_dir/train_mod_cal/$folder/$suffix/$file; done
