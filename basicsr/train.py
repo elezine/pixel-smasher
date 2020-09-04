ab115b57e62eb48eaef8199404cba2de24d1ed1a
Ekaterina Lezine <31071818+elezine@users.noreply.github.com>
2020-09-04 13:36:32 -0400

    Update train.py

--- basicsr/train.py
+++ basicsr/train.py
@@ -9,9 +9,10 @@ from mmcv.runner import get_dist_info, get_time_str, init_dist
 from os import path as osp
 
 import sys
-sys.path.insert(0, '/data_dir/pixel-smasher/basicsr/')
+sys.path.insert(0, '/data_dir/pixel-smasher/basicsr/data')
 
-from basicsr.data import create_dataloader, create_dataset
+from data import create_dataloader, create_dataset
+print('got this far')
 from basicsr.data.data_sampler import EnlargedSampler
 from basicsr.data.prefetch_dataloader import CPUPrefetcher, CUDAPrefetcher
 from basicsr.models import create_model
