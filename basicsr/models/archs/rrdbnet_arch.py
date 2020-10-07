fa14348bbf4c11133b4939dc3bb7e1fb018e3f88
Ekaterina Lezine <31071818+elezine@users.noreply.github.com>
2020-10-07 15:30:15 -0400

    Update rrdbnet_arch.py

--- basicsr/models/archs/rrdbnet_arch.py
+++ basicsr/models/archs/rrdbnet_arch.py
@@ -106,7 +106,7 @@ class RRDBNet(nn.Module):
         feat = feat + body_feat
         # upsample
         feat = self.lrelu(
-            self.conv_up1(F.interpolate(feat, scale_factor=2, mode='nearest')))
+            self.conv_up1(F.interpolate(feat, scale_factor=5, mode='nearest')))
         feat = self.lrelu(
             self.conv_up2(F.interpolate(feat, scale_factor=2, mode='nearest')))
         out = self.conv_last(self.lrelu(self.conv_hr(feat)))
