53a887279298db46a7d5e219d3465cb5fe468273
Ethan (milestones) <ethan_kyzivat@brown.edu>
2020-10-07 16:40:47 +0000

    Further updates to local-threshold method. Accedently added classifier.py to prev commit!

--- old_BasicSR/codes/classification/classifier_test.py
+++ old_BasicSR/codes/classification/classifier_test.py
@@ -10,7 +10,7 @@ from multiprocessing import Pool
 import multiprocessing as mp
 import pickle
 import pandas as pd
-from classifier import classify, name_lookup_og_mask, foreground_threshold, buffer_additional, sourcedir_R_mask, sourcedir_R as sourcedir, outdir, apply_radiometric_correction, method
+from classifier import classify, name_lookup_og_mask, foreground_threshold, buffer_additional, sourcedir_R_mask, sourcedir_R as sourcedir, outdir, apply_radiometric_correction, method, water_index_type
 
 
 # example output paths: /data_dir/ClassProject/pixel-smasher/experiments/003_RRDB_ESRGANx4_PLANET/val_images/716222_1368610_2017-08-27_0e0f_BGRN_Analytic_s0984
@@ -20,7 +20,7 @@ from classifier import classify, name_lookup_og_mask, foreground_threshold, buff
 # I/O use to overwrite globals
 sourcedir='/data_dir/hold_mod_shield/HR/x4' # /data_dir/hold_mod
 outdir='/data_dir/other//data_dir/classified_shield_test' #'/data_dir/classified/'
-sourcedir_R_mask='/data_dir/hold_mod_shield_masks/HR/x4'
+sourcedir_R_mask='/data_dir/hold_mod_shield_masks/HR/x4' # need to specify two parent dirs
 # apply_radiometric_correction=False # set to zero if already calibrated
 # foreground_threshold=127
 # buffer_additional=0
@@ -53,13 +53,13 @@ num_files = len(dirpaths)
 # global results
 results = {} # init
 # pool = Pool(1) #mp.cpu_count())
-for i in range(0, num_files): # switch for testing # range(30): # HERE
+for i in range(0, 30): # switch for testing # range(30): num_files
     name = dirpaths[i]
     name_og_mask=name_lookup_og_mask(name)
     print(f'Classifying file: {name}')
     # parallel
     # results[i] = pool.apply(classify, args=(os.path.join(sourcedir, name), os.path.join(outdir, name), thresh, None))# , , callback=collect_result
-    results[i]=classify(os.path.join(sourcedir, name), os.path.join(outdir, name), thresh, None, method=method, og_mask_pth_in= os.path.join(sourcedir_R_mask, name_og_mask))
+    results[i]=classify(os.path.join(sourcedir, name), os.path.join(outdir, name), thresh, None, method=method, og_mask_pth_in= os.path.join(sourcedir_R_mask, name_og_mask), water_index_type=water_index_type)
 # pool.close()
 # pool.join()
 print('All subprocesses done.')
