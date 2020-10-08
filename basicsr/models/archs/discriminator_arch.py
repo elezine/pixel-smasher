d98b851ef03868a56c12a86cb87e529eb19561d9
Ekaterina Lezine <31071818+elezine@users.noreply.github.com>
2020-10-08 17:48:27 -0400

    Update discriminator_arch.py

--- basicsr/models/archs/discriminator_arch.py
+++ basicsr/models/archs/discriminator_arch.py
@@ -181,3 +181,103 @@ class VGGStyleDiscriminator320(nn.Module):
         feat = self.lrelu(self.linear1(feat))
         out = self.linear2(feat)
         return out
+
+class VGGStyleDiscriminator160(nn.Module):
+    """VGG style discriminator with input size 160 x 160.
+
+    It is used to train SRGAN and ESRGAN.
+
+    Args:
+        num_in_ch (int): Channel number of inputs. Default: 3.
+        num_feat (int): Channel number of base intermediate features.
+            Default: 80.
+    """
+
+    def __init__(self, num_in_ch, num_feat):
+        super(VGGStyleDiscriminator160, self).__init__()
+
+        self.conv0_0 = nn.Conv2d(num_in_ch, num_feat, 3, 1, 1, bias=True)
+        self.conv0_1 = nn.Conv2d(num_feat, num_feat, 4, 2, 1, bias=False)
+        self.bn0_1 = nn.BatchNorm2d(num_feat, affine=True)
+
+        self.conv1_0 = nn.Conv2d(num_feat, num_feat * 2, 3, 1, 1, bias=False)
+        self.bn1_0 = nn.BatchNorm2d(num_feat * 2, affine=True)
+        self.conv1_1 = nn.Conv2d(
+            num_feat * 2, num_feat * 2, 4, 2, 1, bias=False)
+        self.bn1_1 = nn.BatchNorm2d(num_feat * 2, affine=True)
+
+        self.conv2_0 = nn.Conv2d(
+            num_feat * 2, num_feat * 4, 3, 1, 1, bias=False)
+        self.bn2_0 = nn.BatchNorm2d(num_feat * 4, affine=True)
+        self.conv2_1 = nn.Conv2d(
+            num_feat * 4, num_feat * 4, 4, 2, 1, bias=False)
+        self.bn2_1 = nn.BatchNorm2d(num_feat * 4, affine=True)
+
+        self.conv3_0 = nn.Conv2d(
+            num_feat * 4, num_feat * 8, 3, 1, 1, bias=False)
+        self.bn3_0 = nn.BatchNorm2d(num_feat * 8, affine=True)
+        self.conv3_1 = nn.Conv2d(
+            num_feat * 8, num_feat * 8, 4, 2, 1, bias=False)
+        self.bn3_1 = nn.BatchNorm2d(num_feat * 8, affine=True)
+
+        self.conv4_0 = nn.Conv2d(
+            num_feat * 8, num_feat * 8, 3, 1, 1, bias=False)
+        self.bn4_0 = nn.BatchNorm2d(num_feat * 8, affine=True)
+        self.conv4_1 = nn.Conv2d(
+            num_feat * 8, num_feat * 8, 4, 2, 1, bias=False)
+        self.bn4_1 = nn.BatchNorm2d(num_feat * 8, affine=True)
+
+        self.linear1 = nn.Linear(num_feat * 8 * 5 * 5, 100)
+        self.linear2 = nn.Linear(100, 1)
+
+        # activation function
+        self.lrelu = nn.LeakyReLU(negative_slope=0.2, inplace=True)
+
+    def forward(self, x):
+        assert x.size(2) == 160 and x.size(3) == 160, (
+            f'Input spatial size must be 160x160, '
+            f'but received {x.size()}.')
+
+        feat = self.lrelu(self.conv0_0(x))
+        feat = self.lrelu(self.bn0_1(
+            self.conv0_1(feat)))  # output spatial size: (80, 80)
+        
+        #print('size of feat after conv0_1 in discriminator:')
+        #print(feat.size())
+
+        feat = self.lrelu(self.bn1_0(self.conv1_0(feat)))
+        feat = self.lrelu(self.bn1_1(
+            self.conv1_1(feat)))  # output spatial size: (40, 40)
+        
+        #print('size of feat after conv1_1 in discriminator:')
+        #print(feat.size())
+
+        feat = self.lrelu(self.bn2_0(self.conv2_0(feat)))
+        feat = self.lrelu(self.bn2_1(
+            self.conv2_1(feat)))  # output spatial size: (20, 20)
+        
+        #print('size of feat after conv2_1 in discriminator:')
+        #print(feat.size())
+
+        feat = self.lrelu(self.bn3_0(self.conv3_0(feat)))
+        feat = self.lrelu(self.bn3_1(
+            self.conv3_1(feat)))  # output spatial size: (10, 10)
+        
+        #print('size of feat after conv3_1 in discriminator:')
+        #print(feat.size())
+
+        feat = self.lrelu(self.bn4_0(self.conv4_0(feat)))
+        feat = self.lrelu(self.bn4_1(
+            self.conv4_1(feat)))  # output spatial size: (5, 5)
+
+        #print('size of feat after conv4_1 in discriminator:')
+        #print(feat.size())
+        
+        feat = feat.view(feat.size(0), -1)
+        
+        #print('size of feat after feat.view(feat.size(0), -1) in discriminator:')
+        #print(feat.size())
+        
+        feat = self.lrelu(self.linear1(feat))
+        out = self.linear2(feat)
+        return out
