1b5a729646ea0f22e5bc09ada69dd4f37748dcdd
Ekaterina Lezine <31071818+elezine@users.noreply.github.com>
2020-10-13 14:54:54 -0400

    Update recreate_cropped.py

--- recreate_cropped.py
+++ recreate_cropped.py
@@ -70,10 +70,16 @@ def worker(HR_path, cropped_SR_folder, cropped_suffix, save_folder, crop_sz, ste
                 crop_img = HR_img[x:x + crop_sz, y:y + crop_sz, :]
             crop_img = np.ascontiguousarray(crop_img)
             if ~np.any(np.sum(crop_img,axis=2)==0): # if all three bands == 0
+                
                 SR_path = str(cropped_SR_folder) + HR_name.replace('.tif', '_s{:04d}_'.format(index)) + str(cropped_suffix) + '.png'
                 print('SR path is : ' + SR_path)
-                SR_image = cv2.imread(SR_path, cv2.IMREAD_UNCHANGED)
-                print('SR image is shape : ' + str(np.shape(SR_image)))
+                
+                try:
+                    #SR_image = cv2.imread(SR_path, cv2.IMREAD_UNCHANGED)
+                    #print('SR image is shape : ' + str(np.shape(SR_image)))
+                    print(os.path.exists(SR_path))
+                except:
+                    print('image path does not exist')
                 
                 if n_channels == 2:
                     new_img[x:x + crop_sz, y:y + crop_sz] = SR_image[:]
