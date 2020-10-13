2f2f243863e584dbcbd18ad277fdef1a68f51dc7
Ekaterina Lezine <31071818+elezine@users.noreply.github.com>
2020-10-13 11:21:20 -0400

    Update recreate_cropped.py

--- recreate_cropped.py
+++ recreate_cropped.py
@@ -62,9 +62,6 @@ def worker(HR_path, cropped_SR_folder, cropped_suffix, save_folder, crop_sz, ste
     w_space = np.arange(0, w - crop_sz + 1, step)
     if w - (w_space[-1] + crop_sz) > thres_sz:
         w_space = np.append(w_space, w - crop_sz)
-
-    print('h_space : ' + str(h_space))
-    print('w_space : ' + str(w_space))
     
     index = 1
     for x in h_space:
@@ -75,6 +72,7 @@ def worker(HR_path, cropped_SR_folder, cropped_suffix, save_folder, crop_sz, ste
                 crop_img = img[x:x + crop_sz, y:y + crop_sz, :]
             crop_img = np.ascontiguousarray(crop_img)
             if ~np.any(np.sum(crop_img,axis=2)==0): # if all three bands == 0
+                print('amde it here')
                 SR_path = str(cropped_SR_folder) + HR_name.replace('.tif', '_s{:04d}_'.format(index)) + str(cropped_suffix) + '.png'
                 print('SR path is : ' + SR_path)
                 SR_image = cv2.imread(SR_path)
