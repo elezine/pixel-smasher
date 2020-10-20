8c224f0ba04db1fe3a4e751c2acaae39d58b68e6
Ekaterina Lezine <31071818+elezine@users.noreply.github.com>
2020-10-20 11:00:58 -0400

    Update recreate_cropped.py

--- recreate_cropped.py
+++ recreate_cropped.py
@@ -79,6 +79,7 @@ def worker(uncropped_path, cropped_folder, cropped_suffix, save_folder, crop_sz,
             if ~np.any(np.sum(crop_img,axis=2)==0): # if all three bands == 0
                 
                 cropped_path = str(cropped_folder) + uncropped_name.replace('.tif', '_s{:04d}_'.format(index)) + str(cropped_suffix) + '.png'
+                print(cropped_path)
                 
                 try:
                     cropped_img = cv2.imread(cropped_path, cv2.IMREAD_UNCHANGED)
